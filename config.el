8737976b3815de50471e40848b8f0f3053b9acc9
Sean <seanal@gmx.de>
2020-05-19 08:40:55 +0200

    v0.2

--- config.el
+++ config.el
@@ -11,9 +11,9 @@
 
 (setq org-support-shift-select t)
 (setq display-line-numbers-type 'relative)
-(setq evil-move-cursor-back nil)
-(setq evil-escape-key-sequence "jk"
-      evil-escape-delay 0.3)
+(setq evil-move-cursor-back nil)                 ; Escape
+(setq evil-escape-key-sequence "jk"              ; Escape
+      evil-escape-delay 0.3)                     ; Escape
 (custom-set-faces! '(doom-modeline-evil-insert-state :weight bold :foreground "#339CDB"))
 
 (setq-default
@@ -64,6 +64,51 @@
 (setq org-directory "~/org/")
 
 (use-package org-roam-protocol)
+
+(defun ha/org-return (&optional ignore)
+  "Add new list item, heading or table row with RET.
+A double return on an empty element deletes it.
+Use a prefix arg to get regular RET. "
+  (interactive "P")
+  (if ignore
+      (org-return)
+    (cond
+     ;; Open links like usual
+     ((eq 'link (car (org-element-context)))
+      (org-return))
+     ;; lists end with two blank lines, so we need to make sure we are also not
+     ;; at the beginning of a line to avoid a loop where a new entry gets
+     ;; created with only one blank line.
+     ((and (org-in-item-p) (not (bolp)))
+      (if (org-element-property :contents-begin (org-element-context))
+          (org-insert-heading)
+        (beginning-of-line)
+        (setf (buffer-substring
+               (line-beginning-position) (line-end-position)) "")
+        (org-return)))
+     ((org-at-heading-p)
+      (if (not (string= "" (org-element-property :title (org-element-context))))
+          (progn (org-end-of-meta-data)
+                 (org-insert-heading))
+        (beginning-of-line)
+        (setf (buffer-substring
+               (line-beginning-position) (line-end-position)) "")))
+     ((org-at-table-p)
+      (if (-any?
+           (lambda (x) (not (string= "" x)))
+           (nth
+            (- (org-table-current-dline) 1)
+            (org-table-to-lisp)))
+          (org-return)
+        ;; empty row
+        (beginning-of-line)
+        (setf (buffer-substring
+               (line-beginning-position) (line-end-position)) "")
+        (org-return)))
+     (t
+      (org-return)))))
+
+(define-key org-mode-map (kbd "RET")  #'ha/org-return)
 
 (setq org-roam-directory "~/Dropbox/zettelkasten/")
 (setq org-roam-buffer-position 'right)
